0ac0439a2b09d5be73dd2ff289817346c419c9eb
Patrick Lewis <plewis@indeed.com>
2023-02-12 15:17:29 -0600

    gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/rive-vite/",
   plugins: [react()],
   assetsInclude: ["**/*.riv"],
 });
